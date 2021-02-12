b8d675587d707e47728d100ffdbb4fc6df468b3f
smket99 <f20180231@hyderabad.bits-pilani.ac.in>
2021-02-12 11:19:25 +0530

    FE Routes added

---
+++ src/components/student.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+export default class Student extends React.Component{
+  render(){
+    return (
+      <div>
+      Student
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import Student from './components/student'
+import Admin from './components/admin'
+import Login from './components/login'
+import {BrowserRouter as Router, Route,NavLink} from 'react-router-dom'
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <div style={{background:'black',fontSize:'30px',color:'dodgerblue',width:'100%'}}>
+      <NavLink style={{margin:'1em',color:'dodgerblue'}} to="/student">Student</NavLink>
+      <NavLink style={{margin:'1em',color:'dodgerblue'}} to="/admin">Admin</NavLink>
+      <NavLink style={{margin:'1em',color:'dodgerblue'}} to="/login">Login</NavLink>
+      </div>
+      <Route exact path="/" component={App}/>
+      <Route  path="/student" component={Student}/>
+      <Route  path="/admin" component={Admin}/>
+      <Route  path="/login" component={Login}/>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
