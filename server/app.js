691c4a906dc8e539a97d1816cfe3a07a031cefc2
akshit <50762175+akshitkh47612@users.noreply.github.com>
2021-03-09 15:22:36 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,12 +5,43 @@ const port = process.env.port || 3000
 const User = require('server/Database/userschema')
 const bcrypt= require('bcryptjs');
 const { response } = require('express');
+//google auth
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);//smiket add your id as you implemented google auth
 
 const connectDB = require('./Database/connection.js');
 connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+//fetching token from frontend 
+app.post('/login',(req,res)=>{
+  let token = req.body.token;
+  async function verify() {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      // Or, if multiple clients access the backend:
+      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
+  const userid = payload['sub'];
+  // If request specified a G Suite domain:
+  // const domain = payload['hd'];
+    //console.log(payload);
+}
+verify()
+.then(()=>{
+  res.cookie('session-token',token);
+  res.send('success');
+}).catch(console.error);
+  //console.log(token);
+})
+
+app.get('/logout',(req,res)=>{
+  res.clearCookie('session-token');
+  res.redirect('/login');
 })
 
 app.use('/',express.static(path.join(__dirname,'static')))
