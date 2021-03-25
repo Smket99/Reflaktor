dfc3ab44052217474c4ad07a5dcafc86f0fabebb
smket99 <f20180231@hyderabad.bits-pilani.ac.in>
2021-03-25 23:48:29 +0530

    Admin Component added and Home Page updated

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import Back from './back.svg';
 import HorizontalScroll from './HorizontalScroll'
 var delta=1;
 function App() {
-    const containerRef = useRef(null)
-    const onScrollDown=(e)=>{
-      delta=delta*30
-      containerRef.current.scrollLeft-=(delta);
-      alert("hello");
-    }
+    // const containerRef = useRef(null)
+    // const onScrollDown=(e)=>{
+    //   delta=delta*30
+    //   containerRef.current.scrollLeft-=(delta);
+    //   alert("hello");
+    // }
   return (
-    <HorizontalScroll>
-    <div  style={{height:'100vh',overflowY:'hidden'}}>
-      
+    <div style={{overflow:'hidden'}}>
+      <button onClick={()=>window.location.href="/login"} style={{fontFamily:'Righteous',fontSize:'140%',position:'absolute',right:'4%',top:'3.5%',background:'black',color:'white',outline:'none',border:'none',cursor:'pointer',padding:'4px 16px'}}>Login</button>
         <img src={Back} style={{height:'100vh'}}/>
-      
+          <style>
+          @import url('https://fonts.googleapis.com/css2?family=Righteous&display=swap');
+          </style>
     </div>
-    </HorizontalScroll>
   );
 }
 
