1361dc8ab77410c0b4a21e6b29a47b5edcd3d1e9
akshit <50762175+akshitkh47612@users.noreply.github.com>
2021-04-02 00:06:04 +0530

    Update mongoose.js

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,6 +1,12 @@
 const mongoose=require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/hostel-management-api',{
+/*mongoose.connect('mongodb://127.0.0.1:27017/hostel-management-api',{
     useNewUrlParser:true,
     useCreateIndex:true,
-})
+})*/
+const URI = "";
+const connectDB = async()=>{
+    await mongoose.connect(URI,{useUnifiedTopology: true,useNewUrlParser: true});
+    console.log('db connected');
+}
+module.exports = connectDB;
